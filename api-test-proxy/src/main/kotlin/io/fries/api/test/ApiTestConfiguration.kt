4f1f17200386f45d7abb061257079436c1b3c03e
Valentin Fries <contact@fries.io>
2022-11-19 22:35:41 +0100

    features/lib - Isolate WireMock implementation inside the api-test-proxy-wiremock module.

--- api-test-proxy/src/main/kotlin/io/fries/api/test/ApiTestConfiguration.kt
+++ api-test-proxy/src/main/kotlin/io/fries/api/test/ApiTestConfiguration.kt
@@ -7,6 +7,20 @@ import java.time.ZonedDateTime
 @Configuration
 class ApiTestConfiguration {
 
+    /**
+     * The supplier method providing the current time.
+     *
+     * It can be mocked by overriding the bean definition in a test class:
+     *
+     * ```kotlin
+     * @TestConfiguration
+     * class CustomConfiguration {
+     *     @Bean
+     *     @Primary
+     *     fun unixClock() = { ZonedDateTime.parse("1970-01-01T00:00:00Z") }
+     * }
+     * ```
+     */
     @Bean
     fun clock(): () -> ZonedDateTime = ZonedDateTime::now
 }
